6e6fa8dd8eede8673115f3ed1b1d37798e2d29cd
ysu <nukdus@msn.com>
2021-01-24 16:24:36 +0800

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'photos/index'
-  get 'users/index'
+  root 'users#index'
+
+  get 'photos/index/:user_id' => 'photos#index'
+  get 'users/index' => 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
