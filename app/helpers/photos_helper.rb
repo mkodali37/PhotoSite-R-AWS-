adba23f7cdaa0110ad1e67af143237c68850069b
ysu <nukdus@msn.com>
2021-01-24 17:42:18 +0800

    helper method for getting photo s3 url

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,11 @@
+require 'aws-sdk-s3'
 module PhotosHelper
+  def image_public_url(file_name)
+    key = File.join('assets/images/', file_name)
+    obj = Aws::S3::Object.new(
+      bucket_name: ENV['aws_bucket_name'],
+      key: key,
+      region: ENV['aws_region'])
+    obj.public_url
+  end
 end
